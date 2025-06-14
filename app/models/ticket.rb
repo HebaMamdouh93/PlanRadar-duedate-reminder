c009df10670d9f515a88c482e3078e447e3ef610
Heba Mamdouh <hebamamdouh2016@gmail.com>
2025-06-14 17:13:46 +0300

    Add validations to models

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -5,4 +5,8 @@ class Ticket < ApplicationRecord
 
   ##### Enums #####
   enum :status, { open: 0, in_progress: 1, resolved: 2 }
+
+  ##### Validations #####
+  validates :title, :description, :status, :assigned_user_id, presence: true
+  validates :progress, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 100 }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,11 @@
 class User < ApplicationRecord
   ##### Associations #####
   has_many :tickets, foreign_key: :assigned_user_id
+
+  ##### Validations #####
+  validates :name, presence: true
+  validates :email, presence: true, uniqueness: true, format: { with: /\A[^@\s]+@[^@\s]+\z/ }
+
+  validates :time_zone, presence: true, inclusion: { in: ActiveSupport::TimeZone.all.map { |tz| tz.name } }
+  validates :reminder_time, :reminder_interval, presence: true, if: -> { due_date_reminders_enabled == true }
 end
